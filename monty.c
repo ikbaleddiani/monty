0631244323a388c8247e7e9f7562cb7daaba53b0
EDDIANI IKBAL <eddianiiqbal@gmail.com>
2023-06-22 17:06:00 -0700

    check if the file is existe and open it

---
+++ monty.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+* main - main function
+* @argc: number of argument
+* @argv: argument
+*
+* Return: to 0
+*/
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	return (0);
+}
+
